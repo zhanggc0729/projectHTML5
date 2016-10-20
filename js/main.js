6178c1be786f90674074d011e97e0744962be932
zhanggc <zhanggc0729@gmail.com>
2016-10-20 16:24:46 +0800

    编写第一个section，遗留问题：在ie8显示手机式样

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 /**
  * Created by zhanggc on 2016/10/19 0019.
  */
+$(window).scroll(function() {
+    //if (winWidth > 767) {
+    var $scrollHeight = $(window).scrollTop();
+    if ($scrollHeight > 600) {
+        $('#home').slideDown(400);
+    }else{
+        $('#home').slideUp(400);
+    }
+    //}
+
+    //got o top
+    if ($(this).scrollTop() > 200) {
+        $('#go-to-top a').fadeIn('slow');
+    } else {
+        $('#go-to-top a').fadeOut('slow');
+    }
+});
